6366172f4a8bfd1b3f41327263c3f32c34839f51
Nathan Truyts <78803504+Nathtruyts@users.noreply.github.com>
2022-07-25 16:24:33 -0300

    Add files via upload

---
+++ criando dados.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct Human {//tipo de dado personalizado, tudo depois da chave é o seu contrato.
+
+    int number;
+    float height;
+    float heightz;
+
+};
+
+int main(){
+    Human n;
+n.number = 7;
+n.height = 20;
+n.heightz = 200;
+
+return 0;
+}
